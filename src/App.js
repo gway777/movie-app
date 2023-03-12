23726bbb2e276d29a3984d703740075665e6d4ef
apple <magic6420@naver.com>
2023-03-12 15:41:54 +0900

    상수 변수 상태 함수 테스트

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+
+// 상수 변수 함수 상태 
+// const text = "HELLO WORLD"
+
+const [text, setText] = useState("")
+
+//함수 정의(버튼동작)
+const submitHandler = () => {
+  setText("helloworld")
+}
+
+// HTML 자리 
   return (
     <div>
-      <h1>1234
-        hello wo
-      </h1>
+      <h1>{text}</h1>
+      <button onClick={submitHandler}>데이타 입력</button>
+
+  
     </div>
   );
-}
+};
 
 export default App;
