9448e0cd1e471c59cb33c2a914a0ee5c0a5932f2
apple <magic6420@naver.com>
2023-03-12 14:48:45 +0900

    project init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>1234
+        hello wo
+      </h1>
+    </div>
+  );
+}
+
+export default App;
